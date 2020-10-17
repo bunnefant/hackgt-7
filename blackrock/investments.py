2ab9dd64c11d85a6ab5689699ae2d407cfb6ab2b
Rohit Chopra <therohit.chopra71@gmail.com>
2020-10-17 18:36:34 -0400

    Able to get list of valid tickers based on keyword

--- blackrock/investments.py
+++ blackrock/investments.py
@@ -1,10 +1,42 @@
 import requests
+import json
 
-#response = requests.get("https://www.blackrock.com/tools/api-tester/hackathon?apiType=portfolioAnalysis&calculateExpectedReturns=true&calculateExposures=true&calculatePerformance=true&positions=NUS~29.85%7CMMIAX~38.34%7CICAUX~31.82%7C&runJsFunctionEveryTime=true")
+
+def getAdvice(keyword, rows):
+    search_url = "https://www.blackrock.com/tools/hackathon/search-securities"
+    parameters = {
+        'query': keyword,
+        'rows': rows
+    }
+
+    invests = requests.get(search_url, params=parameters)
+    # debug statement
+    if invests.status_code == 200:
+        print('Call successful')
+
+    data = invests.json()
+
+    tickers = []
+    x = 0
+    for n in range(0, rows):
+        try:
+            avail = data['resultMap']['SEARCH_RESULTS'][0]['resultList'][n]['availability']
+
+            ticker = data['resultMap']['SEARCH_RESULTS'][0]['resultList'][n]['ticker']
+
+            country = data['resultMap']['SEARCH_RESULTS'][0]['resultList'][n]['countryCode']
+
+            # dont allow duplicates
+            if tickers.count(ticker) < 1 and country == 'US' and avail == 'public':
+                tickers.append(ticker)
+                print(f'{ticker}  is {avail} in {country}')
+
+        except KeyError:
+            # print('ripperoni')
+            continue
 
 
-portfolio_analysis_request = requests.get("https://www.blackrock.com/tools/hackathon/security-data",
-                                          params={'identifiers': "ticker:AAPL,ticker:MSFT,ticker:RKUNY"})
+getAdvice('tech', 500)
 
-print(portfolio_analysis_request.status_code)
-print(portfolio_analysis_request.json())
+#
+# print(portfolio_analysis_request.json())
