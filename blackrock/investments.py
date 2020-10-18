d3a520a6dff2586a5ddd104c1a6d3d5961730eee
Rohit Chopra <therohit.chopra71@gmail.com>
2020-10-18 02:33:28 -0400

    Finds best tickers based on keyword

--- blackrock/investments.py
+++ blackrock/investments.py
@@ -1,8 +1,12 @@
 import requests
 import json
+import numpy as np
+import math
+import time as t
 
 
-def getAdvice(keyword, rows):
+def getAdvice(keyword, rows, time, risk):
+    beginning = t.perf_counter()
     search_url = "https://www.blackrock.com/tools/hackathon/search-securities"
     parameters = {
         'query': keyword,
@@ -12,7 +16,7 @@ def getAdvice(keyword, rows):
     invests = requests.get(search_url, params=parameters)
     # debug statement
     if invests.status_code == 200:
-        print('Call successful')
+        print('Call to search is successful')
 
     data = invests.json()
 
@@ -20,23 +24,97 @@ def getAdvice(keyword, rows):
     x = 0
     for n in range(0, rows):
         try:
+            # get relevant info from data
             avail = data['resultMap']['SEARCH_RESULTS'][0]['resultList'][n]['availability']
-
             ticker = data['resultMap']['SEARCH_RESULTS'][0]['resultList'][n]['ticker']
-
             country = data['resultMap']['SEARCH_RESULTS'][0]['resultList'][n]['countryCode']
+            score = data['resultMap']['SEARCH_RESULTS'][0]['resultList'][n]['score']
 
             # dont allow duplicates
             if tickers.count(ticker) < 1 and country == 'US' and avail == 'public':
                 tickers.append(ticker)
-                print(f'{ticker}  is {avail} in {country}')
+                # debug
+                # print(f'{ticker}  is {avail} in {country} and has score {score}')
 
+        # happens when one of the keys above is not found
         except KeyError:
             # print('ripperoni')
             continue
 
+    print(str(len(tickers)) +
+          f'  possible investments for {keyword} were found')
 
-getAdvice('tech', 500)
+    # get data for each possible investment
+    performanceData_url = "https://www.blackrock.com/tools/hackathon/performance"
 
-#
-# print(portfolio_analysis_request.json())
+    ticker_list = ','.join(tickers)
+
+    parameters = {
+        'identifiers': ticker_list
+    }
+
+    performance = requests.get(performanceData_url, params=parameters)
+    # debug statement
+    if performance.status_code == 200:
+        print('Call to performance is successful')
+
+    data = performance.json()
+
+    # get risk and sharpe ratio for the right time
+
+    # set years that we wanna invest for
+    temp_years = 1 if time == 1 else 3 if time == 2 else 5
+
+    # debug stuff
+    # print(ticker_list)
+
+    # get the data we need
+    stats = np.zeros((len(tickers), 2))
+    for n in range(0, len(tickers)):
+        # calculate how many years of data we have
+        calc_years = 2020 - \
+            int(str(data['resultMap']['RETURNS'][n]['startDate'])[:4])
+        # years we want to check data for
+        years = min(math.floor((time/3) * calc_years) + 1, temp_years)
+        if years == 1:
+            years = 'one'
+        elif years == 2:
+            years = 'two'
+        elif years == 3:
+            years = 'three'
+        elif years == 4:
+            years = 'four'
+        elif years == 5:
+            years = 'five'
+        try:
+            # year risk
+            stats[n][0] = data['resultMap']['RETURNS'][n][
+                'latestPerf'][f'{years}YearSharpeRatio']
+            # year sharp ratio
+            stats[n][1] = data['resultMap']['RETURNS'][n]['latestPerf'][f'{years}YearRisk']
+        except KeyError:
+            stats[n][0] = -9999999
+            stats[n][1] = -9999999
+
+    # print(stats)
+
+    # calculate best one
+
+    # pick best investments
+    # REMEMBER TO FLIP THIS SHIT OR YOUR WHOLE CODE WILL BE WRONG
+    value = stats[:, 0] - (risk * stats[:, 1])
+    best_tickers = []
+    # get top three choices
+    for i in range(0, 3):
+        best_ind = np.argmax(value)
+        print(
+            f'best option is {tickers[best_ind]} with {stats[best_ind][0]} sharp and {stats[best_ind][1]} risk')
+        best_tickers.append(tickers.pop(best_ind))
+        value = np.delete(value, best_ind)
+
+    print(best_tickers)
+    print(t.perf_counter() - beginning)
+    return(best_tickers)
+
+
+getAdvice('health', 60, 2, 1)
