160872dde9f1e072fc40c30f6dad3e1598b757b6
Girish Hari <bunnefant@gmail.com>
2020-10-18 03:49:18 -0400

    chatbot text cleaning

--- ibm scripts/chatbotBackend.py
+++ ibm scripts/chatbotBackend.py
@@ -3,19 +3,6 @@ import requests
 from cloudant.client import Cloudant
 
 
-import subprocess
-# import sys
-import config
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-install('geopy')
-install('googlemaps')
-
-from geopy.geocoders import Nominatim
-import googlemaps
-from googlemaps import places
-
 
 def getUserAddress():
     client = Cloudant.iam("d02a0070-4a25-4e81-b712-a8e6c38a8863-bluemix", "0CpvlhxnS58tIZMsdu4QuUqw4bai6t1EYcJAv4Mo4lnI")
@@ -28,22 +15,6 @@ def getUserAddress():
     print(userDoc)
     return userDoc['userData']['address']
 
-# print(getUserAddress())
-
-
-def find_closest_foodbank(address):
-    geolocator = Nominatim(user_agent="foodBankLocator")
-    location = geolocator.geocode(address)
-    coordinates = (location.latitude, location.longitude)
-    client = googlemaps.Client(key=config.googleKey)
-    nearby_banks = places.places_nearby(client=client, location=coordinates, radius=24140, keyword='food bank')
-    food_bank_list = []
-    for foodbank in nearby_banks['results']:
-        location_coord = foodbank['geometry']['location']['lat'], foodbank['geometry']['location']['lng']
-        address = geolocator.reverse(str(location_coord[0]) + ", " + str(location_coord[1]))
-        food_bank_list.append(foodbank['name'])
-    print(food_bank_list)
-    return food_bank_list
 
 def getBankInfo():
     client = Cloudant.iam("d02a0070-4a25-4e81-b712-a8e6c38a8863-bluemix", "0CpvlhxnS58tIZMsdu4QuUqw4bai6t1EYcJAv4Mo4lnI")
@@ -55,22 +26,63 @@ def getBankInfo():
     userDoc = db["user1"]
     return userDoc['userData']['accountInformation']
 
+def getFoodBanks():
+    client = Cloudant.iam("d02a0070-4a25-4e81-b712-a8e6c38a8863-bluemix", "0CpvlhxnS58tIZMsdu4QuUqw4bai6t1EYcJAv4Mo4lnI")
+    client.connect()
+    database_name = "user_db"
+    # print(client.all_dbs())
+    db = client[database_name] #open database
+    # print(db)
+    userDoc = db["user1"]
+    return userDoc['foodBanks']
+
+def getUser():
+    client = Cloudant.iam("d02a0070-4a25-4e81-b712-a8e6c38a8863-bluemix", "0CpvlhxnS58tIZMsdu4QuUqw4bai6t1EYcJAv4Mo4lnI")
+    client.connect()
+    database_name = "user_db"
+    # print(client.all_dbs())
+    db = client[database_name] #open database
+    # print(db)
+    userDoc = db["user1"]
+    return userDoc
+
 def main(dict):
     if dict['type'] == 'investment':
         ## do stuff with rohits code
         print('hello')
     elif dict['type'] == 'food_bank':
-        print('hello')
-        address = getUserAddress()
-        list = find_closest_foodbank(address)
-        return {'output' : list}
+        list = getFoodBanks()
+        output = "The food banks nearby your address are: \n"
+        i = 0
+        for x in list:
+            if i > 5:
+                break
+            i += 1
+            output += x + "\n"
+        output += "Look up the names on Google to find their addresses"
+        return {'message' : output}
     elif dict['type'] == 'bank_info':
-        return getBankInfo()
+        info = getBankInfo()
+        output = "Here is your account information. This is a " + info['type'] + " account. The name of the account is " + info['description'] + ". Your account number is " + str(info['accountNumber']) + ". Your current balance is " + str(info['currentBalance']) + ". The interest rate you recieve for this account is " + str(info['interestRate']) + "."
+        return {'message' : output}
     elif dict['type'] == 'billing':
-        ## edit billing or display current billing
-        print('hello')
+        return {'message' : getUser()['userData']['bills']}
     elif dict['type'] == 'budget':
+        return {'message' : getUser()['userData']['budget']}
+    elif dict['type'] == 'transactions':
         ##do stuff with budget, return, change ...
-        print('budget')
-
+        return {'message' : getUser()['userData']['transactions']}
+    elif dict['type'] == 'account_number':
+        ##do stuff with budget, return, change ...
+        return {'message' : getBankInfo()['accountNumber']}
+    elif dict['type'] == 'interest_rate':
+        ##do stuff with budget, return, change ...
+        return {'message' : getBankInfo()['interestRate']}
+    elif dict['type'] == 'account_name':
+        ##do stuff with budget, return, change ...
+        return {'message' : getBankInfo()['description']}
+    elif dict['type'] == 'current_balance':
+        ##do stuff with budget, return, change ...
+        print(getBankInfo()['currentBalance'])
+        return {'message' : getBankInfo()['currentBalance']}
 # main({'type' : 'food_bank'})
